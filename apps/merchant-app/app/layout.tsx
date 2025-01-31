4a17862d459690ca53ef752503fd417e7e0373e9
codingpratham <coding.job17@gmail.com>
2025-01-31 22:55:29 +0530

    first commit

---
+++ apps/merchant-app/app/layout.tsx
@@ -0,0 +1,18 @@
+// import {Inter} from "next/font/google"
+
+// const inter=Inter({subsets:["latin"]})
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
